30b53327f67b51c2921dba452ef0dd3875489f7e
jaoua2 <48482890+jaoua2@users.noreply.github.com>
2023-09-24 15:03:03 +0100

    Add files via upload

---
+++ dynamic_map_cursor.py
@@ -0,0 +1,35 @@
+import folium
+from folium import plugins
+import random
+import time
+
+# Create a map centered around the world
+world_map = folium.Map(location=[0, 0], zoom_start=2)
+
+# Create a marker cluster to hold dynamic markers
+marker_cluster = plugins.MarkerCluster().add_to(world_map)
+
+# Function to generate random data points
+def generate_random_data():
+    return [random.uniform(-180, 180), random.uniform(-90, 90)]
+
+# Update the map with new data points and save it after each update
+while True :  # Update forever
+    location = generate_random_data()
+    #location= [36.8065, 10.1815]
+    # Create a CircleMarker with a red dot and a tooltip showing the latitude and longitude
+    marker = folium.CircleMarker(
+        location=location,
+        radius=5,
+        color='red',
+        fill=True,
+        fill_color='red',
+        fill_opacity=1,
+        tooltip=f"Lat: {location[0]}, Lng: {location[1]}"
+    )
+    marker.add_to(marker_cluster)
+    
+    # Save the map with a unique name for each update
+    world_map.save("dynamic_world_map.html")
+    
+    time.sleep(0.5)  # Sleep for 0.5 seconds before the next update
---
+++ map_csv.py
@@ -0,0 +1,31 @@
+import folium
+from folium import plugins
+import pandas as pd
+
+# Create a map centered around the world
+world_map = folium.Map(location=[0, 0], zoom_start=2)
+
+# Create a marker cluster to hold dynamic markers
+marker_cluster = plugins.MarkerCluster().add_to(world_map)
+
+# Read coordinates from a CSV file
+coordinates_df = pd.read_csv('coordinates.csv')
+
+# Iterate through the CSV data and add markers to the map
+for index, row in coordinates_df.iterrows():
+    location = [row['latitude'], row['longitude']]
+    
+    # Create a CircleMarker with a red dot and a tooltip showing the latitude and longitude
+    marker = folium.CircleMarker(
+        location=location,
+        radius=5,
+        color='red',
+        fill=True,
+        fill_color='red',
+        fill_opacity=1,
+        tooltip=f"Lat: {location[0]}, Lng: {location[1]}"
+    )
+    marker.add_to(marker_cluster)
+
+# Save the map
+world_map.save("csv_coordinates_map.html")
