9a31ea9821e00827aecd1a79d9581c91b40eb1f0
jaoua2 <48482890+jaoua2@users.noreply.github.com>
2023-09-26 07:22:22 +0100

    Update map_csv.py

--- map_csv.py
+++ map_csv.py
@@ -13,7 +13,7 @@ coordinates_df = pd.read_csv('coordinates.csv')
 
 # Iterate through the CSV data and add markers to the map
 for index, row in coordinates_df.iterrows():
-    location = [row['longitude'], row['latitude']]
+    location = [row['latitude'], row['longitude']]
     
     # Create a CircleMarker with a red dot and a tooltip showing the latitude and longitude
     marker = folium.CircleMarker(
@@ -23,7 +23,7 @@ for index, row in coordinates_df.iterrows():
         fill=True,
         fill_color='red',
         fill_opacity=1,
-        tooltip=f"Lng: {location[0]}, Lat: {location[1]}"
+        tooltip=f"Lat: {location[0]}, Long: {location[1]}"
     )
     marker.add_to(marker_cluster)
 
